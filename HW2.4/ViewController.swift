0c1d980dd5e2d3226e3f0aaf6c9a34d2c96c3f0e
VexelB <vexelb@gmail.com>
2021-06-25 21:16:19 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by Евгений Войтин on 25.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
